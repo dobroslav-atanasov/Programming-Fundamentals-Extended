67cae7067d43d338c98186f3e3e45e7b4ebd4430
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2017-04-02 19:03:23 +0300

    Upload Strings and Text Processing - Exercises

---
+++ 14. StringsAndTextProcessing-Exercises/03. JSONParse/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03.JSONParse
+{
+    public class Student
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public List<int> Grades { get; set; }
+    }
+}
---
+++ 14. StringsAndTextProcessing-Exercises/04. SentenceSplit/SentenceSplit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.SentenceSplit
+{
+    public class SentenceSplit
+    {
+        public static void Main()
+        {
+            var sentence = Console.ReadLine();
+            var delimiter = Console.ReadLine();
+
+            var result = sentence.Replace(delimiter, ", ");
+
+            Console.WriteLine($"[{result}]");
+        }
+    }
+}
