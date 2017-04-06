ef3d8fad829f52adbe0c95060111fecc8b75857c
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2017-04-06 11:25:21 +0300

    Upload Regular Expressions - Lab

---
+++ 15. RegularExpressions-Lab/01. ReplaceTag/ReplaceTag.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _01.ReplaceTag
+{
+    public class ReplaceTag
+    {
+        public static void Main()
+        {
+            var text = Console.ReadLine();
+
+            while (text != "end")
+            {
+                var pattern = @"<a.*?href.*?=(.*)>(.*?)<\/a>";
+                var regex = new Regex(pattern);
+
+                var replacement = @"[URL href=$1]$2[/URL]";
+                var result = Regex.Replace(text, pattern, replacement);
+
+                Console.WriteLine(result);
+
+                text = Console.ReadLine();
+            }
+        }
+    }
+}
