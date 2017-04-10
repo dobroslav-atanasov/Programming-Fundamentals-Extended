a3b5e67262b7e69196db4aa74cfcdfbfd9782d08
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2017-04-10 21:27:29 +0300

    Upload Exam Preparation I

---
+++ 16. ExamPreparationI/03. EnduranceRally/EnduranceRally.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.EnduranceRally
+{
+    public class EnduranceRally
+    {
+        public static void Main()
+        {
+            var drivers = Console.ReadLine().Split(' ').ToArray();
+            var zones = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+            var indexes = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            foreach (var driver in drivers)
+            {
+                var symbol = driver.ToCharArray();
+                var startFuel = (int)symbol[0];
+
+                var currentFuel = 0.0;
+                currentFuel += startFuel;
+                var count = 0;
+                
+                for (int i = 0; i < zones.Length; i++)
+                {
+                    count = i;
+                    if (indexes.Contains(i))
+                    {
+                        currentFuel += zones[i];
+                    }
+                    else
+                    {
+                        currentFuel -= zones[i];
+                    }
+
+                    if (currentFuel <= 0)
+                    {
+                        break;
+                    }
+                }
+
+                if (currentFuel > 0)
+                {
+                    Console.WriteLine($"{driver} - fuel left {currentFuel:F2}");
+                }
+                else
+                {
+                    Console.WriteLine($"{driver} - reached {count}");
+                }
+            }
+        }
+    }
+}
