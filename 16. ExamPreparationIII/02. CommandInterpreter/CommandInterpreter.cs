3eb36794a2d22fb0993e7403c07b352e856130ed
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2017-04-19 13:06:31 +0300

    Upload Exam Preparation III

---
+++ 16. ExamPreparationIII/02. CommandInterpreter/CommandInterpreter.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.CommandInterpreter
+{
+    public class CommandInterpreter
+    {
+        public static void Main()
+        {
+            List<string> list = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            var input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                var inputParts = input.Split(' ');
+
+                var command = inputParts[0];
+
+                switch (command)
+                {
+                    case "reverse":
+                        int reverseStart = int.Parse(inputParts[2]);
+                        int reverseCount = int.Parse(inputParts[4]);
+
+                        if (reverseStart >= 0 && reverseStart < list.Count && reverseCount >= 0 && (reverseStart + reverseCount) <= list.Count)
+                        {
+                            Reverse(list, reverseStart, reverseCount);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input parameters.");
+                        }
+                        
+                        break;
+
+                    case "sort":
+                        int sortStart = int.Parse(inputParts[2]);
+                        int sortCount = int.Parse(inputParts[4]);
+
+                        if (sortStart >= 0 && sortStart < list.Count && sortCount >= 0 && (sortStart + sortCount) <= list.Count)
+                        {
+                            Sort(list, sortStart, sortCount);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input parameters.");
+                        }
+
+                        break;
+
+                    case "rollLeft":
+                        int rollLeftTimes = int.Parse(inputParts[1]);
+
+                        if (rollLeftTimes >= 0)
+                        {
+                            RollLeft(list, rollLeftTimes);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input parameters.");
+                        }
+
+                        break;
+
+                    case "rollRight":
+                        int rollRightTimes = int.Parse(inputParts[1]);
+
+                        if (rollRightTimes >= 0)
+                        {
+                            RollRight(list, rollRightTimes);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input parameters.");
+                        }
+
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"[{string.Join(", ", list)}]");
+        }
+
+        private static void RollRight(List<string> list, int rollRightTimes)
+        {
+            for (int i = 0; i < rollRightTimes % list.Count; i++)
+            {
+                var lastElement = list[list.Count - 1];
+
+                for (int j = list.Count - 1; j > 0; j--)
+                {
+                    list[j] = list[j - 1];
+                }
+
+                list[0] = lastElement;
+            }
+        }
+
+        private static void RollLeft(List<string> list, int rollLeftTimes)
+        {
+            for (int i = 0; i < rollLeftTimes % list.Count; i++)
+            {
+                var firstElement = list[0];
+
+                for (int j = 0; j < list.Count - 1; j++)
+                {
+                    list[j] = list[j + 1];
+                }
+
+                list[list.Count - 1] = firstElement;
+            }
+        }
+
+        private static void Sort(List<string> list, int sortStart, int sortCount)
+        {
+            list.Sort(sortStart, sortCount, null);
+        }
+
+        private static void Reverse(List<string> list, int reverseStart, int reverseCount)
+        {
+            list.Reverse(reverseStart, reverseCount);
+        }
+    }
+}
---
+++ 16. ExamPreparationIII/03. RageQuit/RageQuit.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace _03.RageQuit
+{
+    public class RageQuit
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+
+            var pattern = @"(.+?)(\d{1,2})";
+            var regex = new Regex(pattern);
+
+            var matches = regex.Matches(input);
+
+            var sb = new StringBuilder();
+
+            foreach (Match match in matches)
+            {
+                var symbols = match.Groups[1].Value.ToUpper();
+                var repeat = int.Parse(match.Groups[2].Value);
+
+                for (int i = 0; i < repeat; i++)
+                {
+                    sb.Append(symbols);
+                }
+            }
+
+            var result = sb.ToString();
+
+            var list = new List<char>();
+
+            foreach (var symbol in result)
+            {
+                list.Add(symbol);
+            }
+
+            var count = list.Distinct().ToList();
+
+            Console.WriteLine($"Unique symbols used: {count.Count}");
+            Console.WriteLine(result);
+        }
+    }
+}
