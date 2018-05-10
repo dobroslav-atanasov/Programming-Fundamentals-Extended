665d5fd36337cce8703d00cc57576be972eeb3bf
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-05-10 09:59:21 +0300

    Uploaded Exam - 20.10.2016

---
+++ Exam-20.10.2016/01.SweetDessert/StartUp.cs
@@ -0,0 +1,33 @@
+﻿namespace SweetDessert
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var cash = decimal.Parse(Console.ReadLine());
+            var numberOfGuests = int.Parse(Console.ReadLine());
+            var priceOfBananas = decimal.Parse(Console.ReadLine());
+            var priceOfEggs = decimal.Parse(Console.ReadLine());
+            var priceOfBerriesForKilo = decimal.Parse(Console.ReadLine());
+
+            var portions = numberOfGuests / 6;
+            if (numberOfGuests % 6 != 0)
+                portions++;
+
+            var totalMoney = portions * 2 * priceOfBananas + portions * 4 * priceOfEggs +
+                             portions * 0.2M * priceOfBerriesForKilo;
+
+            if (totalMoney <= cash)
+            {
+                Console.WriteLine($"Ivancho has enough money - it would cost {totalMoney:F2}lv.");
+            }
+            else
+            {
+                var needMoney = totalMoney - cash;
+                Console.WriteLine($"Ivancho will have to withdraw money - he will need {needMoney:F2}lv more.");
+            }
+        }
+    }
+}
---
+++ Exam-20.10.2016/04.CubicsMessages/StartUp.cs
@@ -0,0 +1,46 @@
+﻿namespace CubicsMessages
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+
+            while (input != "Over!")
+            {
+                var count = int.Parse(Console.ReadLine());
+
+                var pattern = $@"(^\d+)([A-Za-z]{{{count}}})([^A-Za-z]*$)";
+                var regex = new Regex(pattern);
+
+                var match = regex.Match(input);
+
+                if (match.Success)
+                {
+                    var left = match.Groups[1].Value;
+                    var message = match.Groups[2].Value;
+                    var right = match.Groups[3].Value;
+
+                    var indexes = string.Concat(left, right).Where(char.IsDigit).Select(x => x - '0');
+
+                    var sb = new StringBuilder();
+
+                    foreach (var index in indexes)
+                        if (index < 0 || index >= message.Length)
+                            sb.Append(' ');
+                        else
+                            sb.Append(message[index]);
+
+                    Console.WriteLine($"{message} == {sb}");
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
