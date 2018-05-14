f9bb128c23ed5c0a567ab022c7e5485620f9fb5f
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-05-14 09:30:55 +0300

    Uploaded Exam - 30.04.2017

---
+++ Exam-30.04.2017/01.Wormtest/StartUp.cs
@@ -0,0 +1,35 @@
+﻿namespace Wormtest
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int wormsLength = int.Parse(Console.ReadLine());
+            decimal wormsWidth = decimal.Parse(Console.ReadLine());
+
+            int wormsLengthINCM = wormsLength * 100;
+
+            if (wormsWidth != 0)
+            {
+                decimal reminder = (decimal)wormsLengthINCM % wormsWidth;
+                if (reminder == 0)
+                {
+                    decimal divideZero = wormsWidth * wormsLengthINCM;
+                    Console.WriteLine($"{divideZero:F2}");
+                }
+                else
+                {
+                    decimal percent = (wormsLengthINCM / wormsWidth) * 100;
+                    Console.WriteLine($"{percent:F2}%");
+                }
+            }
+            else
+            {
+                decimal divideZero = wormsWidth * wormsLengthINCM;
+                Console.WriteLine($"{divideZero:F2}");
+            }
+        }
+    }
+}
---
+++ Exam-30.04.2017/03.Wormhole/StartUp.cs
@@ -0,0 +1,31 @@
+﻿namespace Wormhole
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var list = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            var moves = 0;
+
+            for (var i = 0; i < list.Count; i++)
+                if (list[i] == 0)
+                {
+                    moves++;
+                }
+                else
+                {
+                    moves++;
+                    var currentIndex = i;
+                    int currentNumber = list[i];
+                    list[i] = 0;
+                    list[currentNumber] = 0;
+                    i = currentNumber;
+                }
+
+            Console.WriteLine(moves);
+        }
+    }
+}
